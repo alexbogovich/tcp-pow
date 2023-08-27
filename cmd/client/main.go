842205cc132a62289a526f6043be1c6fbb7223a4
Alex Bogovich <bogovich.alexander@gmail.com>
2023-08-27 21:48:04 +0600

    expand client errors

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"os"
 	"tcp-pow/challenge"
@@ -14,30 +15,30 @@ func main() {
 
 	conn, err := net.Dial("tcp", address)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("failed to connect to %s: %w", address, err))
 	}
 
 	buf := make([]byte, 1024)
 	challengeN, err := conn.Read(buf)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("failed to read challenge: %w", err))
 	}
 
 	result, err := challenge.HasherSolver(buf[:challengeN])
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("failed to process challenge: %w", err))
 	}
 
 	_, err = conn.Write(result)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("failed to write solution: %w", err))
 	}
 
 	buf = make([]byte, 256)
 	n, err := conn.Read(buf)
 
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("failed to read quote: %w", err))
 	}
 
 	println(string(buf[:n]))
